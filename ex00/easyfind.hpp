e54dc7d4d9647c120b40279e9e12a3f728dba6c2
Zahra Syyida <zahra.syyida@gmail.com>
2023-12-04 14:04:23 +0400

    start

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,23 @@
 /*   By: zsyyida <zsyyida@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 00:34:48 by zsyyida           #+#    #+#             */
-/*   Updated: 2023/12/03 00:34:49 by zsyyida          ###   ########.fr       */
+/*   Updated: 2023/12/03 20:54:55 by zsyyida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <algorithm>
+
+# define RED "\033[0;31m"
+# define GREEN "\033[38:5:2m"
+# define BLUE    "\033[34m"
+# define YELLOW  "\033[33m"
+# define RESET "\033[0m"
+
+template <typenameTtfind, int I>
+typename T::iterator easyfind(T t, I i)
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,20 @@
 /*   By: zsyyida <zsyyida@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 00:34:53 by zsyyida           #+#    #+#             */
-/*   Updated: 2023/12/03 00:34:54 by zsyyida          ###   ########.fr       */
+/*   Updated: 2023/12/03 20:50:37 by zsyyida          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+std::cout << BLUE << <VECTOR> << std::endl;
+template < class T, class Alloc = allocator<T> > class vector; // generic template
+
+std::cout << RED << <ARRAY> << std::endl;
+template < class T, size_t N > class array;
+
+std::cout << GREEN << <DEQUE> << std::endl;
+template < class T, class Alloc = allocator<T> > class deque;
+
+std::cout << YELLOW << <LIST> << std::endl;
+template < class T, class Alloc = allocator<T> > class list;
